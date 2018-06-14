420d2446acbbb635395b71e066e1b02c1a97acbf
haint <nguyenhaidhcn@gmail.com>
2018-06-15 02:08:25 +0700

    config

---
+++ src/config/ConfigFile.h
@@ -0,0 +1,21 @@
+//
+// Created by HaiNt on 6/15/18.
+//
+
+#ifndef LIQUIBOOK_CONFIG_H
+#define LIQUIBOOK_CONFIG_H
+#include <string>
+#include <map>
+
+
+class ConfigFile {
+    std::map<std::string,std::string> content_;
+
+public:
+    ConfigFile(std::string const& configFile);
+
+    std::string const& Value(std::string const& section, std::string const& entry) const;
+};
+
+
+#endif //LIQUIBOOK_CONFIG_H
