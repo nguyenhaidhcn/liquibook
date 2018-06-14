c64d6a9fda8c2fdf0114d00730c217f53fbd3397
haint <nguyenhaidhcn@gmail.com>
2018-06-15 02:12:19 +0700

    global class

---
+++ global/global.cpp
@@ -0,0 +1,5 @@
+//
+// Created by HaiNt on 6/15/18.
+//
+
+#include "global.h"
---
+++ global/global.h
@@ -0,0 +1,15 @@
+//
+// Created by HaiNt on 6/15/18.
+//
+
+#ifndef LIQUIBOOK_GLOBAL_H
+#define LIQUIBOOK_GLOBAL_H
+
+
+class global
+{
+
+};
+
+
+#endif //LIQUIBOOK_GLOBAL_H
