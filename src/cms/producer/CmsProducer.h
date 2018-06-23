09959c4b9248b693dda7839b0ef1182f51429659
haint <nguyenhaidhcn@gmail.com>
2018-06-24 01:02:16 +0700

    activemq

--- src/cms/producer/CmsProducer.h
+++ src/cms/producer/CmsProducer.h
@@ -5,7 +5,6 @@
 #ifndef LIQUIBOOK_CMSPRODUCER_H
 #define LIQUIBOOK_CMSPRODUCER_H
 
-#include <apr-1.0/apr_pools.h>
 #include <activemq/library/ActiveMQCPP.h>
 #include <decaf/lang/Thread.h>
 #include <decaf/lang/Runnable.h>
@@ -27,128 +26,128 @@
 #include <iostream>
 #include <memory>
 
-using namespace activemq::core;
-using namespace decaf::util::concurrent;
-using namespace decaf::util;
-using namespace decaf::lang;
-using namespace cms;
-using namespace std;
-
-class CmsProducer : public Runnable {
-private:
-
-    Connection* connection;
-    Session* session;
-    Destination* destination;
-    MessageProducer* producer;
-    int numMessages;
-    bool useTopic;
-    bool sessionTransacted;
-    std::string brokerURI;
-
-private:
-
-    CmsProducer(const CmsProducer&);
-    CmsProducer& operator=(const CmsProducer&);
-
-public:
-
-    CmsProducer(const std::string& brokerURI, int numMessages, bool useTopic = false, bool sessionTransacted = false) :
-        connection(NULL),
-        session(NULL),
-        destination(NULL),
-        producer(NULL),
-        numMessages(numMessages),
-        useTopic(useTopic),
-        sessionTransacted(sessionTransacted),
-        brokerURI(brokerURI) {
-    }
-
-    virtual ~HelloWorldProducer(){
-        cleanup();
-    }
-
-    void close() {
-        this->cleanup();
-    }
-
-    virtual void run() {
-
-        try {
-
-            // Create a ConnectionFactory
-            auto_ptr<CmsProducer> connectionFactory(
-                CmsProducer::createCMSConnectionFactory(brokerURI));
-
-            // Create a Connection
-            connection = connectionFactory->createConnection();
-            connection->start();
-
-            // Create a Session
-            if (this->sessionTransacted) {
-                session = connection->createSession(Session::SESSION_TRANSACTED);
-            } else {
-                session = connection->createSession(Session::AUTO_ACKNOWLEDGE);
-            }
-
-            // Create the destination (Topic or Queue)
-            if (useTopic) {
-                destination = session->createTopic("TEST.FOO");
-            } else {
-                destination = session->createQueue("TEST.FOO");
-            }
-
-            // Create a MessageProducer from the Session to the Topic or Queue
-            producer = session->createProducer(destination);
-            producer->setDeliveryMode(DeliveryMode::NON_PERSISTENT);
-
-            // Create the Thread Id String
-            string threadIdStr = Long::toString(Thread::currentThread()->getId());
-
-            // Create a messages
-            string text = (string) "Hello world! from thread " + threadIdStr;
-
-            for (int ix = 0; ix < numMessages; ++ix) {
-                std::auto_ptr<TextMessage> message(session->createTextMessage(text));
-                message->setIntProperty("Integer", ix);
-                printf("Sent message #%d from thread %s\n", ix + 1, threadIdStr.c_str());
-                producer->send(message.get());
-            }
-
-        } catch (CMSException& e) {
-            e.printStackTrace();
-        }
-    }
-
-private:
-
-    void cleanup() {
-
-        if (connection != NULL) {
-            try {
-                connection->close();
-            } catch (cms::CMSException& ex) {
-                ex.printStackTrace();
-            }
-        }
-
-        // Destroy resources.
-        try {
-            delete destination;
-            destination = NULL;
-            delete producer;
-            producer = NULL;
-            delete session;
-            session = NULL;
-            delete connection;
-            connection = NULL;
-        } catch (CMSException& e) {
-            e.printStackTrace();
-        }
-    }
-};
-
-
+//using namespace activemq::core;
+//using namespace decaf::util::concurrent;
+//using namespace decaf::util;
+//using namespace decaf::lang;
+//using namespace cms;
+//using namespace std;
+//
+//class CmsProducer : public Runnable {
+//private:
+//
+//    Connection* connection;
+//    Session* session;
+//    Destination* destination;
+//    MessageProducer* producer;
+//    int numMessages;
+//    bool useTopic;
+//    bool sessionTransacted;
+//    std::string brokerURI;
+//
+//private:
+//
+//    CmsProducer(const CmsProducer&);
+//    CmsProducer& operator=(const CmsProducer&);
+//
+//public:
+//
+//    CmsProducer(const std::string& brokerURI, int numMessages, bool useTopic = false, bool sessionTransacted = false) :
+//        connection(NULL),
+//        session(NULL),
+//        destination(NULL),
+//        producer(NULL),
+//        numMessages(numMessages),
+//        useTopic(useTopic),
+//        sessionTransacted(sessionTransacted),
+//        brokerURI(brokerURI) {
+//    }
+//
+//    virtual ~HelloWorldProducer(){
+//        cleanup();
+//    }
+//
+//    void close() {
+//        this->cleanup();
+//    }
+//
+//    virtual void run() {
+//
+//        try {
+//
+//            // Create a ConnectionFactory
+//            auto_ptr<CmsProducer> connectionFactory(
+//                CmsProducer::createCMSConnectionFactory(brokerURI));
+//
+//            // Create a Connection
+//            connection = connectionFactory->createConnection();
+//            connection->start();
+//
+//            // Create a Session
+//            if (this->sessionTransacted) {
+//                session = connection->createSession(Session::SESSION_TRANSACTED);
+//            } else {
+//                session = connection->createSession(Session::AUTO_ACKNOWLEDGE);
+//            }
+//
+//            // Create the destination (Topic or Queue)
+//            if (useTopic) {
+//                destination = session->createTopic("TEST.FOO");
+//            } else {
+//                destination = session->createQueue("TEST.FOO");
+//            }
+//
+//            // Create a MessageProducer from the Session to the Topic or Queue
+//            producer = session->createProducer(destination);
+//            producer->setDeliveryMode(DeliveryMode::NON_PERSISTENT);
+//
+//            // Create the Thread Id String
+//            string threadIdStr = Long::toString(Thread::currentThread()->getId());
+//
+//            // Create a messages
+//            string text = (string) "Hello world! from thread " + threadIdStr;
+//
+//            for (int ix = 0; ix < numMessages; ++ix) {
+//                std::auto_ptr<TextMessage> message(session->createTextMessage(text));
+//                message->setIntProperty("Integer", ix);
+//                printf("Sent message #%d from thread %s\n", ix + 1, threadIdStr.c_str());
+//                producer->send(message.get());
+//            }
+//
+//        } catch (CMSException& e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//private:
+//
+//    void cleanup() {
+//
+//        if (connection != NULL) {
+//            try {
+//                connection->close();
+//            } catch (cms::CMSException& ex) {
+//                ex.printStackTrace();
+//            }
+//        }
+//
+//        // Destroy resources.
+//        try {
+//            delete destination;
+//            destination = NULL;
+//            delete producer;
+//            producer = NULL;
+//            delete session;
+//            session = NULL;
+//            delete connection;
+//            connection = NULL;
+//        } catch (CMSException& e) {
+//            e.printStackTrace();
+//        }
+//    }
+//};
+//
+//
 
 
 #endif //LIQUIBOOK_CMSPRODUCER_H
