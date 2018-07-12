10d2be49140d53371f0f4ca90c6b96f019a46935
hai <nguyenhaidhcn@gmail.com>
2018-07-12 16:01:09 +0700

    hold thread main

--- src/mt_order_entry/mt_order_entry_main.cpp
+++ src/mt_order_entry/mt_order_entry_main.cpp
@@ -74,170 +74,174 @@ int main(int argc, const char * argv[])
 
     start_cms();
 
-    bool done = false;
-    bool prompt = true;
-    bool interactive = true;
-    bool fileActive = false;
-    std::ostream * log = &std::cout;
-    std::ifstream commandFile;
-    std::ofstream logFile;
-
-    DLOG(INFO) << "Test market ";
-
-
-
-    if(argc > 1)
-    {
-        std::string filename = argv[1];
-        if(filename != "-")
-        {
-            commandFile.open(filename);
-            if(!commandFile.good())
-            {
-                std::cerr << "Can't open command file " << filename << ". Exiting." << std::endl;
-                return -1;
-            }
-            interactive = false;
-            fileActive = true;
-        }
-    }
-    if(argc > 2)
-    {
-        const char * filename = argv[2];
-        logFile.open(filename);
-        if(!logFile.good())
-        {
-            std::cerr << "Can't open log file " << filename << ". Exiting." << std::endl;
-            return -1;
-        }
-        log = & logFile;
-    }
-
-    orderentry::Market * market;
-    market = new orderentry::Market(log);
-
-
-    while( !done)
-    {
-        std::string input;
-        if(fileActive)
-        {
-            std::getline(commandFile, input);
-            if(!commandFile.good())
-            {
-                if(interactive)
-                {
-                    input = "# Switching to console input.";
-                    fileActive = false;
-                }
-                else
-                {
-                    input = "# end of command file.";
-                    done = true;
-                }
-            }
-            // if it came from a file, echo it to the log
-            if(input.substr(0,2) != "##") // don't log ## comments.
-            {
-                *log << input << std::endl;
-            }
-        }
-        else
-        {
-            if(prompt)
-            {
-                DLOG(INFO) << "Action[" << orderentry::Market::prompt()
-                          << "\t(?)    help for more options and detail.\n"
-                          << "\t(Quit) ]\n";
-                prompt = false;
-            }
-            DLOG(INFO) << "> " << std::flush;
-            std::getline(std::cin, input);
-        }
-//        std::transform(input.begin(), input.end(), input.begin(), toupper);
-
-        std::locale loc;
-//        std::string str="Test String.\n";
-        for (std::string::size_type i=0; i<input.length(); ++i)
-            input[i] = std::toupper(input[i],loc);
-
-        if(log != &std::cout && !fileActive)
-        {
-            if(input.substr(0,2) != "##") // don't log ## comments.
-            {
-                *log << input << std::endl;
-            }
-        }
-
-        // if input ends in a ';' be sure there's a space before it to simplify parsing.
-        if(input.length() > 1)
-        {
-            if(input.back() == ';')
-            {
-                input.pop_back();
-                if(input.back() == ' ')
-                {
-                    input.pop_back();
-                }
-                input.append(" ;");
-            }
-        }
-
-        std::vector< std::string> words;
-        orderentry::split(input," \t\v\n\r", words);
-        if(!words.empty())
-        {
-            const std::string command = words[0];
-            if(command == "QUIT")
-            {
-                done = true;
-            }
-            else if(command[0] == '#')
-            {
-                // nothing
-            }
-            else if(command == "F" || command == "FILE")
-            {
-                if(fileActive)
-                {
-                    DLOG(INFO) << "Only one input file at a time can be open." << std::endl;
-                }
-                else
-                {
-                    DLOG(INFO) << "Command file name: " << std::flush;
-                    std::string filename;
-                    std::getline(std::cin, filename);
-                    commandFile.open(filename);
-                    if(commandFile.good())
-                    {
-                        fileActive = true;
-                    }
-                    else
-                    {
-                        DLOG(INFO) << "Cannot open " << filename << std::endl;
-                    }
-                }
-            }
-            else if(command == "?" || command == "HELP")
-            {
-                market->help();
-                *log << "(F)ile  Open or Close a command input file\n"
-                     << "\tArguments\n"
-                     << "\t\t<FileName>  Required if no file is open. Must not appear if file is open.\n";
-                *log << "QUIT  Exit from this program.\n";
-                bool prompt = true;
-            }
-            else if(!market->apply(words))
-            {
-                std::cerr << "Cannot process command";
-                for(auto word = words.begin(); word != words.end(); ++ word)
-                {
-                    std::cerr << ' ' << *word;
-                }
-                std::cerr << std::endl;
-                bool prompt = true;
-            }
-        }
-    }
+    getchar();
+    
+//    bool done = false;
+//    bool prompt = true;
+//    bool interactive = true;
+//    bool fileActive = false;
+//    std::ostream * log = &std::cout;
+//    std::ifstream commandFile;
+//    std::ofstream logFile;
+//
+//    DLOG(INFO) << "Test market ";
+//
+//
+//
+//    if(argc > 1)
+//    {
+//        std::string filename = argv[1];
+//        if(filename != "-")
+//        {
+//            commandFile.open(filename);
+//            if(!commandFile.good())
+//            {
+//                std::cerr << "Can't open command file " << filename << ". Exiting." << std::endl;
+//                return -1;
+//            }
+//            interactive = false;
+//            fileActive = true;
+//        }
+//    }
+//    if(argc > 2)
+//    {
+//        const char * filename = argv[2];
+//        logFile.open(filename);
+//        if(!logFile.good())
+//        {
+//            std::cerr << "Can't open log file " << filename << ". Exiting." << std::endl;
+//            return -1;
+//        }
+//        log = & logFile;
+//    }
+//
+//
+//
+//    orderentry::Market * market;
+//    market = new orderentry::Market(log);
+//
+//
+//    while( !done)
+//    {
+//        std::string input;
+//        if(fileActive)
+//        {
+//            std::getline(commandFile, input);
+//            if(!commandFile.good())
+//            {
+//                if(interactive)
+//                {
+//                    input = "# Switching to console input.";
+//                    fileActive = false;
+//                }
+//                else
+//                {
+//                    input = "# end of command file.";
+//                    done = true;
+//                }
+//            }
+//            // if it came from a file, echo it to the log
+//            if(input.substr(0,2) != "##") // don't log ## comments.
+//            {
+//                *log << input << std::endl;
+//            }
+//        }
+//        else
+//        {
+//            if(prompt)
+//            {
+//                DLOG(INFO) << "Action[" << orderentry::Market::prompt()
+//                          << "\t(?)    help for more options and detail.\n"
+//                          << "\t(Quit) ]\n";
+//                prompt = false;
+//            }
+//            DLOG(INFO) << "> " << std::flush;
+//            std::getline(std::cin, input);
+//        }
+////        std::transform(input.begin(), input.end(), input.begin(), toupper);
+//
+//        std::locale loc;
+////        std::string str="Test String.\n";
+//        for (std::string::size_type i=0; i<input.length(); ++i)
+//            input[i] = std::toupper(input[i],loc);
+//
+//        if(log != &std::cout && !fileActive)
+//        {
+//            if(input.substr(0,2) != "##") // don't log ## comments.
+//            {
+//                *log << input << std::endl;
+//            }
+//        }
+//
+//        // if input ends in a ';' be sure there's a space before it to simplify parsing.
+//        if(input.length() > 1)
+//        {
+//            if(input.back() == ';')
+//            {
+//                input.pop_back();
+//                if(input.back() == ' ')
+//                {
+//                    input.pop_back();
+//                }
+//                input.append(" ;");
+//            }
+//        }
+//
+//        std::vector< std::string> words;
+//        orderentry::split(input," \t\v\n\r", words);
+//        if(!words.empty())
+//        {
+//            const std::string command = words[0];
+//            if(command == "QUIT")
+//            {
+//                done = true;
+//            }
+//            else if(command[0] == '#')
+//            {
+//                // nothing
+//            }
+//            else if(command == "F" || command == "FILE")
+//            {
+//                if(fileActive)
+//                {
+//                    DLOG(INFO) << "Only one input file at a time can be open." << std::endl;
+//                }
+//                else
+//                {
+//                    DLOG(INFO) << "Command file name: " << std::flush;
+//                    std::string filename;
+//                    std::getline(std::cin, filename);
+//                    commandFile.open(filename);
+//                    if(commandFile.good())
+//                    {
+//                        fileActive = true;
+//                    }
+//                    else
+//                    {
+//                        DLOG(INFO) << "Cannot open " << filename << std::endl;
+//                    }
+//                }
+//            }
+//            else if(command == "?" || command == "HELP")
+//            {
+//                market->help();
+//                *log << "(F)ile  Open or Close a command input file\n"
+//                     << "\tArguments\n"
+//                     << "\t\t<FileName>  Required if no file is open. Must not appear if file is open.\n";
+//                *log << "QUIT  Exit from this program.\n";
+//                bool prompt = true;
+//            }
+//            else if(!market->apply(words))
+//            {
+//                std::cerr << "Cannot process command";
+//                for(auto word = words.begin(); word != words.end(); ++ word)
+//                {
+//                    std::cerr << ' ' << *word;
+//                }
+//                std::cerr << std::endl;
+//                bool prompt = true;
+//            }
+//        }
+//    }
     return 0;
 }
